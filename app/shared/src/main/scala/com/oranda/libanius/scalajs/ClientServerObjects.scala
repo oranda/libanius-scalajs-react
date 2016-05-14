8027c6c11d0633db903c9dfe9b1c9908895748df
oranda <james@oranda.com>
2016-05-14 22:45:49 +0200

    Updated copyright message to 2016.

--- app/shared/src/main/scala/com/oranda/libanius/scalajs/ClientServerObjects.scala
+++ app/shared/src/main/scala/com/oranda/libanius/scalajs/ClientServerObjects.scala
@@ -1,6 +1,6 @@
 /*
  * Libanius
- * Copyright (C) 2012-2015 James McCabe <james@oranda.com>
+ * Copyright (C) 2012-2016 James McCabe <james@oranda.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
