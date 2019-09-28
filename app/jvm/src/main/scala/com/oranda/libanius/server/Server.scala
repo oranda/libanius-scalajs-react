d5a4ae844b84d8e426dc1702ed9ec300096df91b
James McCabe <jjtmccabe@gmail.com>
2019-09-28 15:44:55 +0100

    Update year in copyright notice

--- app/jvm/src/main/scala/com/oranda/libanius/server/Server.scala
+++ app/jvm/src/main/scala/com/oranda/libanius/server/Server.scala
@@ -1,6 +1,6 @@
 /*
  * Libanius
- * Copyright (C) 2012-2018 James McCabe <james@oranda.com>
+ * Copyright (C) 2012-2019 James McCabe <james@oranda.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
